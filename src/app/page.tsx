244a8099a586bd6b8de7e80a06886a67674c1654
lekan.dev <realjoy9@gmail.com>
2023-08-31 23:42:53 +0100

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,24 @@
+"use client"
+import { motion } from "framer-motion";
+
 
 export default function Home() {
-  return <h1> hello and welcome to Olalekan Abdulfatah Portfolio</h1>
+  return <div style={{
+    backgroundColor:'red'
+  }}> hello and welcome to Olalekan Abdulfatah Portfolio
+
+<motion.div
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+  />
+   <motion.div
+    animate={{
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    }}
+  />
+
+  </div>
 }
